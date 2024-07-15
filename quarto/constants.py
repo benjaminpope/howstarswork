523eb6f97c4a27f153f7b17de14cde22f552f7f2
benjaminpope <bjspope@gmail.com>
2024-07-15 20:17:42 +1000

    constants have their own file

---
+++ quarto/constants.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+G = 6.6743e-11 # N m2 kg-2; Newton's Constant
+k_B = 1.380649e-23 # J/K; Boltzmann's Constant
+m_p = 1.67262192e-27 # kg; proton mass
+
+M_sun = 1.9884e30 # kg
+R_sun = 6.957e8 # m
+Teff_sun = 5780 # effective temperature, K
