23d8e3cce5228d1f3ad6d1236f82f009f9cbf904
benjaminpope <bjspope@gmail.com>
2024-07-21 13:40:49 +1000

    commented lectures

--- quarto/constants.py
+++ quarto/constants.py
@@ -5,10 +5,14 @@ from helpers import * # misc functions
 G = 6.6743e-11 # N m2 kg-2; Newton's Constant
 k_B = 1.380649e-23 # J/K; Boltzmann's Constant
 m_p = 1.67262192e-27 # kg; proton mass
+c = 299792458 # m/s
 
 M_sun = 1.9884e30 # kg
 R_sun = 6.957e8 # m
+L_sun = 3.86e26 # W
 Teff_sun = 5780 # effective temperature, K
 
 M_earth = 5.972e24 # kg
 R_earth = 6371e3 # m 
+
+year = 365.25 * 24 * 3600
--- quarto/helpers.py
+++ quarto/helpers.py
@@ -1,9 +1,9 @@
 from IPython.display import Markdown
 
-def md_float(f):
-    float_str = "{0:.2g}".format(f)
+def md_float(f, digits=2):
+    float_str = "{0:.{1}g}".format(f, digits)
     if "e" in float_str:
         base, exponent = float_str.split("e")
         return Markdown(r"${0} \times 10^{{{1}}}$".format(base, int(exponent)))
     else:
-        return float_str
+        return Markdown(float_str)
